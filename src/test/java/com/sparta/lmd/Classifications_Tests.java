026a3e5ccc2b1e985f4b3187254c22ea466b89ef
lanyamd2 <110177169+lanyamd2@users.noreply.github.com>
2023-05-02 18:51:06 +0100

    Update Classifications_Tests to pass

--- src/test/java/com/sparta/lmd/Classifications_Tests.java
+++ src/test/java/com/sparta/lmd/Classifications_Tests.java
@@ -8,26 +8,26 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class Classifications_Tests {
     @ParameterizedTest
-    @ValueSource(ints = {5,10,12})
+    @ValueSource(ints = {5,10,11})
     @DisplayName("checks film classifications for children under 12")
     void checkUnderTwelve(int age){
         Assertions.assertEquals("U & PG films are available.",FilmClassifications.availableClassifications(age));
     }
     @ParameterizedTest
-    @ValueSource(ints = {12,14,15})
+    @ValueSource(ints = {12,14,13})
     @DisplayName("checks film classifications for children between 12 and 14")
     void checkBetweenTwelveAndFourteen(int age){
         Assertions.assertEquals("U, PG, 12A & 12 films are available.",FilmClassifications.availableClassifications(age));
     }
     @ParameterizedTest
-    @ValueSource(ints = {15,17,18})
+    @ValueSource(ints = {15,17,16})
     @DisplayName("checks film classifications for children between 15 and 17")
     void checkBetweenFifteenAndSeventeen(int age){
         Assertions.assertEquals("U, PG, 12A, 12 & 15 films are available.",FilmClassifications.availableClassifications(age));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {14,18,63})
+    @ValueSource(ints = {19,18,63})
     @DisplayName("checks film classifications for people over 18")
     void checkAboveEighteen(int age){
         Assertions.assertEquals("All films are available.", FilmClassifications.availableClassifications(age));
