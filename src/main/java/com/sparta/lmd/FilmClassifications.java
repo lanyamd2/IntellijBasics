2a7a3d8c35f75472822d87956f6ac5183a381617
lanyamd2 <laniangel12@gmail.com>
2023-05-02 17:25:31 +0100

    updated FilmClassifications and Classification_Tests

--- src/main/java/com/sparta/lmd/FilmClassifications.java
+++ src/main/java/com/sparta/lmd/FilmClassifications.java
@@ -1,2 +1,28 @@
-package com.sparta.lmd;public class FilmClassifications {
+package com.sparta.lmd;
+
+public class FilmClassifications {
+    public static void main(String[] args) {
+        System.out.println(availableClassifications(12));
+    }
+    public static String availableClassifications(int ageOfViewer)
+    {
+        String result = "";
+
+        if(ageOfViewer>0 && ageOfViewer<12){//8
+            result = "U & PG films are available.";
+        }
+        else if (ageOfViewer >= 12 && ageOfViewer<15)//12
+        {
+            result = "U, PG, 12A & 12 films are available.";
+        }
+        else if (ageOfViewer >= 15 && ageOfViewer<18)
+        {
+            result = "U, PG, 12A, 12 & 15 films are available.";
+        }
+        else if(ageOfViewer>=18)
+        {
+            result = "All films are available.";
+        }
+        return result;
+    }
 }
--- src/test/java/com/sparta/lmd/Classifications_Tests.java
+++ src/test/java/com/sparta/lmd/Classifications_Tests.java
@@ -1,2 +1,37 @@
-package com.sparta.lmd;public class Classifications_Tests {
+package com.sparta.lmd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class Classifications_Tests {
+    @ParameterizedTest
+    @ValueSource(ints = {5,10,12})
+    @DisplayName("checks film classifications for children under 12")
+    void checkUnderTwelve(int age){
+        Assertions.assertEquals("U & PG films are available.",FilmClassifications.availableClassifications(age));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {12,14,15})
+    @DisplayName("checks film classifications for children between 12 and 14")
+    void checkBetweenTwelveAndFourteen(int age){
+        Assertions.assertEquals("U, PG, 12A & 12 films are available.",FilmClassifications.availableClassifications(age));
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {15,17,18})
+    @DisplayName("checks film classifications for children between 15 and 17")
+    void checkBetweenFifteenAndSeventeen(int age){
+        Assertions.assertEquals("U, PG, 12A, 12 & 15 films are available.",FilmClassifications.availableClassifications(age));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {14,18,63})
+    @DisplayName("checks film classifications for people over 18")
+    void checkAboveEighteen(int age){
+        Assertions.assertEquals("All films are available.", FilmClassifications.availableClassifications(age));
+    }
+
+
 }
